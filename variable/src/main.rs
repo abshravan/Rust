5a04563703268e08052c5eea55e3cdf39643529c
A B Shravan Krishna <abshravankrishna@gmail.com>
2023-10-16 11:48:17 +0530

    program implementing varible declatration

---
+++ variable/src/main.rs
@@ -0,0 +1,6 @@
+fn main() {
+    let x =4; //declare x as 4 implsic type(can't cange value trougout program)
+    let y: u32 =5; //declare y as 5 unsigned 32 bit integer
+    println!("x is: {}",x); //print x inside the curly bracket
+       
+    }
